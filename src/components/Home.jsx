7d8008a17631ecb18b4dfa210b2b13475a6a6981
Kevin  Lara <133811075+KevinLara11@users.noreply.github.com>
2024-01-22 19:59:56 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import FotoDev from "../assets/fotodev.png"
-/* import { MdOutlineKeyboardArrowRight } from "react-icons/md"; */
+import { MdOutlineKeyboardArrowRight } from "react-icons/md"; 
 import { Link } from "react-scroll";
 
 const Home = () => {
@@ -32,9 +32,9 @@ const Home = () => {
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
             >
               Project
-   {/*            <span className="group-hover:rotate-90 duration-300">
+              <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
-              </span> */}
+              </span> 
             </Link>
           </div>
         </div>
