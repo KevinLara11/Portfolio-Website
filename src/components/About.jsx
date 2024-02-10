177d75b7de148d3ac8f7807f30f82a9f1c4f3275
Kevin  Lara <133811075+KevinLara11@users.noreply.github.com>
2024-02-10 00:50:30 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,7 @@ const About = () => {
         <br />
 
         <p className="text-xl">
-        Soy un Programador web full stack junior autodidacta con 1 año de experiencia trabajando de manera independiente en el estudio y aplicación de tecnologías web.  <br /> <br />
+        Soy un Desarrollador web full stack junior autodidacta con 1 año de experiencia trabajando de manera independiente en el estudio y aplicación de tecnologías web.  <br /> <br />
         Me apasiona profundizar en nuevas tecnologías y aportar al desarrollo de proyectos.<br /> <br />
         Soy una persona proactiva y con ganas de crecer tanto profesional como personalmente, y poseo habilidades para el trabajo en equipo..
         </p>
