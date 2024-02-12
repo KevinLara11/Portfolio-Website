ab664975d69de0d49864dee3a782fdeffae22548
Kevin  Lara <133811075+KevinLara11@users.noreply.github.com>
2024-02-12 01:29:21 -0500

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
-import Curriculum  from "../assets/Curriculum Vitae.pdf"
+/* import Curriculum  from "../assets/Curriculum Vitae.pdf" */
 const SocialLinks = () => {
   const links = [
     {
@@ -33,7 +33,7 @@ const SocialLinks = () => {
       ),
       href: "mailto:kevinlaracaicedo98@gmail.com",
     },
-    {
+/*     {
       id: 4,
       child: (
         <>
@@ -42,7 +42,7 @@ const SocialLinks = () => {
       ),
       href: Curriculum,
       download: true,
-    },
+    }, */
   ];
 
   return (
