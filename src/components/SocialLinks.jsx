ab20c67e994328cd3bfb49c47a093c9ec3af4443
Kevin  Lara <133811075+KevinLara11@users.noreply.github.com>
2024-02-23 02:05:00 -0300

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+/* import { FaGithub, FaLinkedin } from "react-icons/fa"; */
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 /* import Curriculum  from "../assets/Curriculum Vitae1.pdf"  */
