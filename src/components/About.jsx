96e44fb8d5dd85f7a9bc4bd24aabe98c103f9a7c
Kevin  Lara <133811075+kevinlara16@users.noreply.github.com>
2024-06-04 00:26:35 -0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,10 +16,10 @@ const About = () => {
         <br />
 
         <p className="text-xl">
-        Soy un programador autodidacta y de rápido aprendizaje, con una mentalidad proactiva y comprometida estoy constantemente enfrentando nuevos desafíos y bien predispuesto para el trabajo en equipo.<br /> <br /> 
+        Soy un desarrollador autodidacta y de rápido aprendizaje, con una mentalidad proactiva y comprometida estoy constantemente enfrentando nuevos desafíos y bien predispuesto para el trabajo en equipo.<br /> <br /> 
         Me considero una persona curiosa, proactiva, comprometida y con ganas de crecer tanto profesional como personalmente.
         Además, tengo la capacidad de ser una persona que se adapta y aprende de cualquier tema en muy poco tiempo. Estoy dispuesto a aprender algo nuevo y mejorar mis habilidades constantemente. <br /> <br /> 
-        En busca de mi primera experiencia como Programador Full Stack, estoy emocionado de explorar nuevas oportunidades en tecnologías. Aunque me adapto a otras tecnologías, tengo una gran pasión por el backend y creo que puedo aportar un valor significativo a su equipo.... 
+        En busca de mi primera experiencia como Desarrollador Full Stack, estoy emocionado de explorar nuevas oportunidades en tecnologías. Aunque me adapto a otras tecnologías, tengo una gran pasión por el backend y creo que puedo aportar un valor significativo a su equipo.... 
         </p>
       </div>
     </div>
